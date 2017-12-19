4a902c914d27dbca62ccaa68b51382bb90ab189b
zhaowichen <zhaowichen@192.168.1.3>
2017-12-19 17:16:23 +0800

    Initial commit

---
+++ Yiyi.py
@@ -0,0 +1,22 @@
+x=1+2
+#print(x)
+#print('x')
+sum=0
+for x in range(101):
+    sum=sum+x
+#print (sum)
+n=99
+while n>0:
+    sum=sum-n
+    n=n-1
+#print (sum)
+d = {'Michael': 952, 'Bob': 75, 'Tracy': 85}
+#print(d['Michael'])
+#print(abs(-100))
+def mm(x):
+    if x>=5:
+        return x
+    else:
+        return -x
+for x in range(10):
+    print (mm(x))
